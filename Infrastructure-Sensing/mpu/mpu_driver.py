57713095478829439444bdc3671c95ebc77c3239
RPCS IS HW Team <mgang@andrew.cmu.edu>
2022-02-28 15:44:48 -0500

    mpu is able to collect data. Still need to tune the parameters

--- Infrastructure-Sensing/mpu/mpu_driver.py
+++ Infrastructure-Sensing/mpu/mpu_driver.py
@@ -18,7 +18,7 @@ GYRO_ZOUT_H  = 0x47
 
 
 # Initialize MPU
-def MPU_Init():
+def mpu_init():
 	#write to sample rate register
 	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
 	
@@ -34,7 +34,7 @@ def MPU_Init():
 	#Write to interrupt enable register
 	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
-def read_raw_data(addr):
+def mpu_read_data(addr):
 	#Accelero and Gyro value are 16-bit
         high = bus.read_byte_data(Device_Address, addr)
         low = bus.read_byte_data(Device_Address, addr+1)
@@ -47,6 +47,35 @@ def read_raw_data(addr):
                 value = value - 65536
         return value
 
-def main():
-    
-    return
+
+bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+Device_Address = 0x68   # MPU6050 device address
+
+mpu_init()
+
+print (" Reading Data of Gyroscope and Accelerometer")
+
+while True:
+	
+	#Read Accelerometer raw value
+	acc_x = mpu_read_data(ACCEL_XOUT_H)
+	acc_y = mpu_read_data(ACCEL_YOUT_H)
+	acc_z = mpu_read_data(ACCEL_ZOUT_H)
+	
+	#Read Gyroscope raw value
+	gyro_x = mpu_read_data(GYRO_XOUT_H)
+	gyro_y = mpu_read_data(GYRO_YOUT_H)
+	gyro_z = mpu_read_data(GYRO_ZOUT_H)
+	
+	#Full scale range +/- 250 degree/C as per sensitivity scale factor
+	Ax = acc_x/16384.0
+	Ay = acc_y/16384.0
+	Az = acc_z/16384.0
+	
+	Gx = gyro_x/131.0
+	Gy = gyro_y/131.0
+	Gz = gyro_z/131.0
+	
+
+	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
+	sleep(1)
