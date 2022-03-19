f4e9f528909ed1befafab557fe7a399807cdcfb9
Fan Bu <fbu@linux-28.andrew.cmu.edu>
2022-03-19 15:32:53 -0400

    initial GPS

--- Personal-Systems/data_collection.py
+++ Personal-Systems/data_collection.py
@@ -7,11 +7,13 @@ import sys
 script_dir     = os.path.dirname(__file__)
 IMU_dir        = os.path.join(script_dir, 'LSM6DS33')
 button_dir     = os.path.join(script_dir, 'rpcs_pdhw_button')
+gps_dir        = os.path.join(script_dir, 'rpcs_pdhw_gps')
 sys.path.append(IMU_dir)
 sys.path.append(button_dir)
+sys.path.append(gps_dir)
 
 from MinIMU_v5_pi import MinIMU_v5_pi
-import button
+import button, gps
 
 #GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
@@ -41,19 +43,27 @@ if __name__ == "__main__":
 	IMU = MinIMU_v5_pi()
 	IMU.enableAccel_Gyro(0,0)
 
+	gps.initGPS()
+
 	cur_time = 0
 
 	while cur_time < NUM_SAMPLES * SLEEPTIME: # getting hundred thousand samples
 
-		if (button.button_pressed()):
-			row = [cur_time, not_flat, IMU.readAccelerometer(), IMU.readGyro()]
-			writer.writerow(row)
+		gps_valid = gps.getGPS()
+
+		bumpiness = 0
+		if button.button_pressed():
+			bumpiness = not_flat
 		else:
-			row = [cur_time, flat, IMU.readAccelerometer(), IMU.readGyro()]
-			writer.writerow(row)
+			bumpiness = flat
 		
-		#print(row)
+		if gps_valid:
+			row = [cur_time, bumpiness, IMU.readAccelerometer(), IMU.readGyro(), 1]
+		else:
+			row = [cur_time, bumpiness, IMU.readAccelerometer(), IMU.readGyro()]
+		#writer.writerow(row)
+		
+		print(row)
 
 		cur_time += SLEEPTIME
 		time.sleep(SLEEPTIME)
-        print("done")
